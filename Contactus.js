6f6dd4351bb9d20cb648c31fbdd515643d67d7f3
CaitlinMcLintock <115666965+CaitlinMcLintock@users.noreply.github.com>
2022-12-01 18:42:23 +0000

    Add files via upload

---
+++ Contactus.js
@@ -0,0 +1,5 @@
+let userInput = prompt("Sign up to our Weekly Newsletter and get 15% off your next purchase!");
+
+if (InputDeviceInfo){
+    alert("Thanks! You will recieve your discount code in your next email.");
+}
